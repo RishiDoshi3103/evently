63185badbca6181ec72e5c8cbee5f4f270bf00f0
Rishi Doshi <99541663+RishiDoshi3103@users.noreply.github.com>
2023-12-16 23:09:25 +0530

    more changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import CategoryFilter from '@/components/shared/CategoryFilter'
+import Search from '@/components/shared/Search'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -30,8 +32,8 @@ export default function Home() {
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
       <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
       <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search 
-          CategoryFilter
+          <Search /> 
+          <CategoryFilter />
         </div>
       </section>
     </>
