1fbb63a126818df36c73e932edc53b6cb772af79
Rishi Doshi <99541663+RishiDoshi3103@users.noreply.github.com>
2023-12-16 23:19:23 +0530

    z

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,33 +1,14 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
-const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
- 
-// FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new Error("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
- 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
-} satisfies FileRouter;
- 
-export type OurFileRouter = typeof ourFileRouter;
+import * as z from "zod"
+
+export const eventFormSchema = z.object({
+  title: z.string().min(3, 'Title must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters').max(400, 'Description must be less than 400 characters'),
+  location: z.string().min(3, 'Location must be at least 3 characters').max(400, 'Location must be less than 400 characters'),
+  imageUrl: z.string(),
+  startDateTime: z.date(),
+  endDateTime: z.date(),
+  categoryId: z.string(),
+  price: z.string(),
+  isFree: z.boolean(),
+  url: z.string().url()
+})
